f5fa782ded39d8f4670ff7c7d537acee15fcd647
marwah16 <mba.sdotid@gmail.com>
2021-02-28 11:44:04 +0000

    test upload

---
+++ a.sh
@@ -0,0 +1,7 @@
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install sublime-text
+sudo apt install python3-pip
+sudo python3 -m pip install pyautogui
+
